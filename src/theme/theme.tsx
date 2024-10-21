6fc4320d53ea794ed3d71481a6e1875d3646e05b
kaiqueZpriv1 <kakamelo141205@gmail.com>
2024-10-20 21:13:12 -0300

    criado padrão de projeto, com fontes tamanhos e larguras

---
+++ src/theme/theme.tsx
@@ -0,0 +1,25 @@
+export const theme = {
+	colors: {
+		background: '#edf6f9',
+		font: '#252422',
+		secondary: '#2a9d8f',
+	},
+	source: {
+		primary: '"Montserrat", sans-serif',
+		secondary: '"Roboto", sans-serif',
+	},
+	size: {
+		xs: '0.75rem',
+		sm: '0.875rem',
+		base: '1rem',
+		lg: '1.125rem',
+		xl: '1.25rem',
+		xxl: '1.5rem',
+		xxxl: '3rem',
+	},
+	weight: {
+		light: 300,
+		normal: 400,
+		bold: 700,
+	},
+};
